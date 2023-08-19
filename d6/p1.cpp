a3aa8da4ddef253121bc64012de32f325541ee2a
Maximilian R <maxi.rostock@outlook.de>
2023-08-19 05:05:53 +0200

    day 6

---
+++ d6/p1.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <cctype>
+#include <cstdint>
+#include <iostream>
+#include <map>
+#include <regex>
+#include <set>
+#include <string>
+#include <vector>
+
+int main() {
+  std::string str;
+  std::cin >> str;
+  size_t block_begin = 0;
+  for (size_t i = 0; i < str.length(); ++i) {
+    for (size_t j = block_begin; j < i; ++j) {
+      if (str[i] == str[j]) {
+        block_begin = j + 1;
+      }
+
+      if (i - block_begin >= 4) {
+        std::cout << i;
+        return 0;
+      }
+    }
+  }
+}
